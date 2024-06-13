90a603a2ac0ddac762b3005c32f52fa0a1ff6c0c
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-06-13 20:22:30 +0500

    chore: Update StatusCard component styling
    This commit updates the StatusCard component by adjusting the height and styling of the parent div element. The changes include setting a fixed height for different screen sizes and adding a flex container to center the content. These updates improve the layout and appearance of the StatusCard component.

--- src/components/StatusCard/StatusCard.jsx
+++ src/components/StatusCard/StatusCard.jsx
@@ -5,8 +5,9 @@ const StatusCard = () => {
     let status = true
   return (
     <>
-     <div className='bg_table px-8 py-10 lg:w-[50%] rounded-[20px]'>
-          <div className='flex justify-between '>
+     <div className='bg_table px-8 h-[255px] md:h-[223px] lg:h-[255px]  rounded-[20px] flex justify-center items-center'>
+      <div className='w-full'>
+          <div className='flex justify-between items-center'>
             <h1 className='text-2xl '>Status</h1>
             {status ? <div className='flex items-center gap-2'><img src={images.close} alt="" className='w-5 h-6' /><span className='text-xl'>non registered</span></div>: <div className='flex items-center gap-2'><img src={images.tick} alt="" className='w-5' /><span className='text-xl'>registered</span></div>}
           </div>
@@ -15,11 +16,12 @@ const StatusCard = () => {
             {status ? <div className='flex items-center gap-1'><span className='text-xl'>0</span><img src={images.logo} alt="" className='w-7 h-7' /></div>: <div className='flex items-center gap-1'><span className='text-xl'>2000</span><img src={images.logo} alt="" className='w-7 h-7' /></div>}
           </div>
           <div className='flex justify-center mt-5'>
-            {status ? <h1 className='text-xl font-light'>Stake $WUFI to be eligible</h1>: <h1 className='text-xl font-light'>Stake more $WUFI to increase your allocation</h1>}
+            {status ? <h1 className='text-lg text-gray-400 font-light'>Stake $WUFI to be eligible</h1>: <h1 className='text-lg text-gray-400 font-light'>Stake more $WUFI to increase your allocation</h1>}
           </div>
           <div className='flex justify-center mt-5'>
-            <Button name={"Stake $WUFI"} style={"font-semibold "}/>
+            <Button name={"Stake $WUFI"} style={"px-16"}/>
           </div>
+      </div>
      </div>
     </>
   )
